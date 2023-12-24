4db540c49e8bfc37c6aefe1ee2cdad2f55e5d2a9
membrain12 <39191142+membrain12@users.noreply.github.com>
2023-12-24 06:53:14 -0600

    Start working on function to display books

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const addBookBtn = document.querySelector('#addBook');
 const dialog = document.querySelector('dialog');
+const bookContainer = document.querySelector('.book-container');
 
 addBookBtn.addEventListener('click', addBookToLibrary);
 
@@ -12,10 +13,32 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
+const hp = new Book("Harry Potter and the Sorceror's Stone", "JK Rowling", 1200, true);
+const hp2 = new Book("Harry Potter and the Chamber of Secrets", "JK Rowling", 1200, true);
+
+myLibrary.push(hp);
+myLibrary.push(hp2);
+
 function addBookToLibrary() {
   dialog.showModal();
 }
 
 function displayBooks() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = document.createElement('div');
+    let title = document.createElement('p')
+    let author = document.createElement('p');
+    let pages = document.createElement('p');
+    let read = document.createElement('button');
 
+    title.textContent = myLibrary[i].title;
+    author.textContent = myLibrary[i].author;
+    book.appendChild(title);
+    book.appendChild(author);
+
+    book.style.class
+    bookContainer.appendChild(book);
+  }
 }
+
+displayBooks();
